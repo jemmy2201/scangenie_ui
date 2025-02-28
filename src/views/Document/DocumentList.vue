a9c0c68ef5c82fa4281fb5b575da1aba60810435
jemmy <cchaliansyah@gmail.com>
2025-02-28 10:31:01 +0700

    step 2.1 list DocumentList.vue

--- src/views/Document/DocumentList.vue
+++ src/views/Document/DocumentList.vue
@@ -11,13 +11,13 @@
                         :items="items"
                         :items-per-page="5"
                         item-value="id"
-                        class="elevation-1"
                 >
-                        <!-- Kolom Aksi -->
-                        <template item.actions="{ item }">
-                                <v-icon class="mr-2" color="primary" @click="viewItem(item)">mdi-eye</v-icon>
-                                <v-icon class="mr-2" color="warning" @click="editItem(item)">mdi-pencil</v-icon>
-                                <v-icon color="red" @click="deleteItem(item)">mdi-delete</v-icon>
+                        <template v-slot:item.actions="{ item }">
+                                <div class="text-right">
+                                        <v-icon class="mr-2" color="primary" @click="viewItem(item)">mdi-eye</v-icon>
+                                        <v-icon class="mr-2" color="warning" @click="editItem(item)">mdi-pencil</v-icon>
+                                        <v-icon color="red" @click="deleteItem(item)">mdi-delete</v-icon>
+                                </div>
                         </template>
                 </v-data-table>
         </v-container>
@@ -37,7 +37,6 @@
         // Header Tabel
         const headers = [
                 { text: "Filename", key: "filename" },
-                { text: "Extracted Info", key: "extracted_info" },
                 { text: "Actions", key: "actions", sortable: false }
         ];
 
@@ -49,8 +48,7 @@
                 try {
                         const response = await api.post("/extract/list"); // Sesuaikan URL API
                         items.value = response.data.results;
-                        console.log('jrg',items)
-                        console.log('jrg 1',response.data.results)
+                        console.log("Data diterima:", JSON.stringify(items.value, null, 2));
                 } catch (error) {
                         console.error("Error fetching data:", error);
                         snackbar.value.message = "Gagal mengambil data!";
